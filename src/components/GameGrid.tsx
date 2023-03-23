2ebf85e26392f6b881c235dd759d3f346e111ed2
Malleshkumar G R <malleshkumargr.code@gmail.com>
2023-03-23 15:46:24 +0530

    Refactor: remove duplicated styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,19 +3,31 @@ import React, { useEffect, useState } from "react";
 import useGames from "../hooks/useGames";
 import apiClient from "../services/api-client";
 import GameCard from "./GameCard";
+import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
   const { error, games, isLoading } = useGames();
-  const skeletons = [1, 2,3,4,5,6]
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5}} spacing={10} padding="10px">
-        { isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={10}
+        padding="10px"
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
