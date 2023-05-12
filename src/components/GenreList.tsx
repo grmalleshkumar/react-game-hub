782194ec8a15ffa4929d0fbf8a7e537b7faac799
Malleshkumar G R <malleshkumargr.code@gmail.com>
2023-05-12 17:38:26 +0530

    Refactoring - Fetch games with react-query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,51 +5,54 @@ import {
   Image,
   List,
   ListItem,
-  Spinner,
-  Text,
+  // Spinner,
+  // Text,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre,selectedGenre }: Props) => {
-  const { data, isLoading, error } = useGenres();
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
+  const { data, error, isLoading } = useGenres();
 
   if (error) return null;
-  if (isLoading) return <Spinner />;
+
+  // if (isLoading) return <Spinner />;
 
   return (
     <>
-    <Heading fontSize={'2xl'} marginBottom={3}>Genres</Heading>
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-              objectFit="cover"
-            //   marginRight={8}
-            />
-            <Button
-              fontSize="lg"
-              variant="link"
-              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
-              onClick={() => onSelectGenre(genre)}
-              whiteSpace={'normal'}
-              textAlign={'left'}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+      <Heading fontSize={"2xl"} marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {data.results?.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+                objectFit="cover"
+                //   marginRight={8}
+              />
+              <Button
+                fontSize="lg"
+                variant="link"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                whiteSpace={"normal"}
+                textAlign={"left"}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 };
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -5,20 +5,25 @@ import { Platform } from "../hooks/useGames";
 import usePlatforms from "../hooks/usePlatforms";
 
 interface Props {
-    onSelectPlatform: (platform: Platform) => void;
-    selectedPlatform: Platform | null
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform}: Props) => {
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   const { data } = usePlatforms();
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || 'Platforms' }
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
-          <MenuItem key={platform.id} onClick={() => onSelectPlatform(platform)}>{platform.name}</MenuItem>
+        {data?.results?.map((platform) => (
+          <MenuItem
+            key={platform.id}
+            onClick={() => onSelectPlatform(platform)}
+          >
+            {platform.name}
+          </MenuItem>
         ))}
       </MenuList>
     </Menu>
